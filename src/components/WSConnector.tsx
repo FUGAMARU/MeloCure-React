bf50c11ce5de81f94f3ea7f170e48e8facd503a8
FUGAMARU <gofugashi@gmail.com>
2023-02-05 13:14:06 +0900

    [Update] WebSocketの接続先アドレスの変更

--- src/components/WSConnector.tsx
+++ src/components/WSConnector.tsx
@@ -1,12 +1,12 @@
-export const socket = new WebSocket("wss://melocure.fugamaru.com:8082");
+export const socket = new WebSocket("wss://melocure.fugamaru.com/ws/");
 socket.addEventListener("open", e => {
     console.log("==========WebSocket Connected==========");
-    socket.send(JSON.stringify([{"type": "rfq"}]));
+    socket.send(JSON.stringify([{ "type": "rfq" }]));
 });
 socket.addEventListener("message", e => {
     const received = JSON.parse(e.data)[0];
-    if(received["type"] == "init"){
-        const tmpQueue = [{"type": "init", "online": received.online}, ...received["queue"]];
+    if (received["type"] == "init") {
+        const tmpQueue = [{ "type": "init", "online": received.online }, ...received["queue"]];
         sessionStorage.setItem("prevQueue", JSON.stringify(tmpQueue));
     }
 });
